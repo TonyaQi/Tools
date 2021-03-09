376ac7982e30278398fd28fb3fa85ce179a84d97
qitong <qitong@nibirutech.com>
2021-03-09 16:58:20 +0800

    feat: add zhushi

--- 继承/index.js
+++ 继承/index.js
@@ -3,9 +3,11 @@ function parent () {
 }
 
 function children () {
+    // call 的时候就把parent中的语句执行一遍，执行this.fname = 时 ，children也有了
     parent.call(this);
     this.lName = 'c'
 }
 
+// 继承父函数的方法
 children.prototype = new parent();
 
